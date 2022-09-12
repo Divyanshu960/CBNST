f703e94ba124188746a40f57ae107eb151137c26
Divyanshu Chaurasiya <72182690+Divyanshu960@users.noreply.github.com>
2022-09-12 17:47:57 +0530

    Create mathfunctions.c

---
+++ mathfunctions.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    printf("Functions used in Math.h ");
+    printf("Sqrt %f\n",sqrt(10.0));
+    printf("Exp %f\n",exp(4.0));
+    printf("Log %f\n",log(4.0));
+    printf("Log10 %f\n",log10(100.0));
+    printf("Fabs %f\n",fabs(-5.2));
+    printf("Ceil %f\n",ceil(4.5));
+    printf("Floor %f\n",floor(-4.5));
+    printf("Pow %f\n",pow(4.0,.5));
+    printf("Fmod %f\n",fmod(4.5,2.0));
+    printf("Sin %f\n",sin(0.0));
+    printf("Cos %f\n",cos(0.0));
+    printf("Tan %f\n",tan(0.0));
+    return 0;
+}
